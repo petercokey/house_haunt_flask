6b50ad42deb21b86ea158704ff2b97c7ba8bf1b4
Okezue Chukwuebuka Peter <petercokey96@gmail.com>
2025-11-10 07:42:24 -0800

    Update auth.py and extensions.py to MongoDB, remove SQLAlchemy

--- app/extensions.py
+++ app/extensions.py
@@ -1,15 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
+# app/extensions.py
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
-from flask_login import LoginManager
-from flask_jwt_extended import JWTManager  # ✅ Add this line
 
-# === Initialize extensions globally (no app bound yet) ===
-db = SQLAlchemy()
 bcrypt = Bcrypt()
 mail = Mail()
-login_manager = LoginManager()
-jwt = JWTManager()  # ✅ Add this line
-
-
-
