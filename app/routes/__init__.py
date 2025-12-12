a5afa8b082ca9971b3c268f5f801ef3929cda719
Okezue Chukwuebuka Peter <petercokey96@gmail.com>
2025-12-12 07:09:53 -0800

    update modification on __init__.py route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,15 @@
 ﻿
+from . import auth
+from . import contact
+from . import wallet
+from . import review
+from . import agent
+from . import haunter
+from . import kyc
+from . import dashboard
+from . import notifications
+from . import favorites
+from . import seed
+from . import transactions
+from . import static_files
+from . import admin
