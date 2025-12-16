e303790f4d0dab222ac0ec789881aaa364a9824a
Okezue Chukwuebuka Peter <petercokey96@gmail.com>
2025-12-16 06:43:22 -0800

    standardize static_files blueprint to bp

--- app/routes/static_files.py
+++ app/routes/static_files.py
@@ -1,9 +1,14 @@
+# app/routes/static_files.py
 from flask import Blueprint, send_from_directory, current_app
 import os
 
-bp_static = Blueprint("static_files", __name__)
+bp = Blueprint(
+    "static_files",
+    __name__,
+    url_prefix="/uploads"
+)
 
-@bp_static.route("/uploads/house_images/<path:filename>")
-def serve_house_image(filename):
-    folder = os.path.join(current_app.root_path, "uploads", "house_images")
-    return send_from_directory(folder, filename)
+@bp.route("/<path:filename>")
+def serve_upload(filename):
+    upload_folder = os.path.join(current_app.root_path, "uploads")
+    return send_from_directory(upload_folder, filename)
