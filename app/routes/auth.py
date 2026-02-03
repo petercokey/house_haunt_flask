06c5b4784e04f22a884d95c69482416793cc9bfd
Okezue Chukwuebuka Peter <petercokey96@gmail.com>
2026-02-03 06:08:45 -0800

    fixed forgot-password and reset password in auth.me

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-﻿from flask import Blueprint, request, jsonify, g, requests
+﻿from flask import Blueprint, request, jsonify, g
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 from datetime import datetime, timedelta
