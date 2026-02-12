514ef69f172a865975ab4252380558d676c0e00e
Okezue Chukwuebuka Peter <petercokey96@gmail.com>
2026-02-12 04:13:41 -0800

    fix  routes  run.py and requirements.txt

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
-﻿from app import create_app
+﻿import eventlet
+eventlet.monkey_patch()
+
+from app import create_app
+from app.extensions import socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    socketio.run(app)
+
 
